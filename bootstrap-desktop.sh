e712a60ebf6ea98d7d93673718a5257b2856eb75
MarkusB <>
2024-09-13 15:44:36 +0200

    fixes

--- bootstrap-desktop.sh
+++ bootstrap-desktop.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# If you have a protected environment, just activate a venv upfront (first command only needed once):
+# $ python3 -m venv ~/venv
+# $ . ~/venv/bin/activate
+
 xcode-select --install
 python3 -m pip install --upgrade pip
 python3 -m pip install ansible
+# use --force to upgrade
 ansible-galaxy install -r requirements.yml
 ansible-playbook main.yml -K
