fd5763e96a952644a63f3e791f3abbac72aaf5d6
MarkusB <>
2024-11-29 12:40:43 +0100

    bootstrap-desktop.sh

--- bootstrap-desktop.sh
+++ bootstrap-desktop.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 if ! (pip >/dev/null && pip -V) ; then
   python3 -m venv ~/venv
   echo "Not in a virtual environment. Exiting."
@@ -9,8 +7,11 @@ if ! (pip >/dev/null && pip -V) ; then
 fi
 
 xcode-select --install
+
+set -e
+
 python3 -m pip install --upgrade pip
 python3 -m pip install ansible
 # use --force to upgrade
 ansible-galaxy install -r requirements.yml
-ansible-playbook main.yml -K
+ansible-playbook main.yml -K #--check --diff
